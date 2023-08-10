b09a8731a8f6c94acba24b2b7731982f9728d402
Zot3 <139402089+Zot3@users.noreply.github.com>
2023-08-10 14:17:25 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,22 +1,22 @@
 def __init__ (self,name):
-  self.accocunt_name = John
+  self.accocunt_name = name
   self.account_balance = 0
+  
 def deposit(self,amount):
-  self.amount = int(input())
   if self.amount <=0:
-    pass
     return False
 else:
    self.account_balance += amount
    return True
+  
 def withdraw(self,amount):
-    self.amount = int(input())
-    if self.amount <= 0 or self.amount > self.account balance:
-      pass
+    if amount <= 0 or amount > self.account_balance:
       return False
     else:
-      self.account_balance -= self.amount
+        self.account_balance -= amount
+      
 def getbalance(self):
     return self.account_balance
+  
 def get_name(self):
     return self.account_name
